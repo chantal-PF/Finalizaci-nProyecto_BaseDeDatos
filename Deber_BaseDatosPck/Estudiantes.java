43306f568b9b70a0613ccdcf7249d5e69b866990
chantal-PF <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2025-02-17 17:04:55 -0500

    Update Estudiantes.java

--- Deber_BaseDatosPck/Estudiantes.java
+++ Deber_BaseDatosPck/Estudiantes.java
@@ -4,31 +4,30 @@ public class Estudiantes {
 
 		public String id;
 		public String nombre;
-		public String apellidos;
-		public int edad;
+		public String apellido;
+		public String edad;
 		public String carrera;
 	    public String semestre;
 	    
-		public Estudiantes(String id, String nombre, String apellidos, int edad, String carrera, String semestre) {
+		public Estudiantes() {
+			super();
+		}
+	    
+	    public Estudiantes(String id, String nombre, String apellido, String edad, String carrera, String semestre) {
 			super();
 			this.id = id;
 			this.nombre = nombre;
-			this.apellidos = apellidos;
+			this.apellido = apellido;
 			this.edad = edad;
 			this.carrera = carrera;
 			this.semestre = semestre;
 		}
 
-		public Estudiantes() {
-			super();
-		}
-
-
-
+	
 		public void Imprimir() {
 			System.out.println("Id: "+ this.id);
 			System.out.println("Nombre: "+ this.nombre);
-			System.out.println("Apellidos: "+ this.apellidos);
+			System.out.println("Apellidos: "+ this.apellido);
 			System.out.println("Edad: "+ this.edad);
 			System.out.println("Carrera: "+ this.carrera);
 			System.out.println("Semestre: "+ this.semestre);
@@ -36,3 +35,4 @@ public class Estudiantes {
 		
 
 }
+
