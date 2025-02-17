f7b1f8133a5101ebedbb65977d920e39a51b0da4
chantal-PF <chantal.penafiel@tecnologicoliceocristiano.edu.ec>
2025-02-17 17:05:24 -0500

    Update Main.java

--- Deber_BaseDatosPck/Main.java
+++ Deber_BaseDatosPck/Main.java
@@ -3,15 +3,14 @@ package Deber_BaseDatosPck;
 public class Main {
 
 	public static void main(String[] args) {
-		Estudiantes objEstudiante = new Estudiantes("0955253679","Chantal Xiomara", "Peñafiel Fernandez", 19, "Desarrollo de software", "2do semestre");
+		Estudiantes objEstudiante = new Estudiantes("955453987","Carla Xiomara", "Perez Fernandez", "18", "Desarrollo de software", "4to semestre");
 		BaseDatos  objBaseDatos = new BaseDatos();
 		
 		
-		objBaseDatos.getEstudiante("0955253679").Imprimir();
+		objBaseDatos.getEstudiante("955453987").Imprimir();
 		
 		
 	}
 
 }
-
 
